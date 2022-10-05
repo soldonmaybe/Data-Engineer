bf5d90332a4d535e87ab9e93005cc777ac014460
Fakhriansyah Wijaya <107629397+soldonmaybe@users.noreply.github.com>
2022-10-05 20:27:35 +0700

    modify script

--- Practice Case 7/producer_twt.py
+++ Practice Case 7/producer_twt.py
@@ -1,47 +1,63 @@
-import tweepy
-from kafka import KafkaProducer
-import logging
+# def keys
+bearer_token = r'AAAAAAAAAAAAAAAAAAAAAELuegEAAAAAcCUcFpnbVW4pt95lMZZ3xrNuTLQ%3DPxnK3Zs5gOMwRXpDBmwqZNxEhrfta0r7cgRK6smPaJd9w265T8'
+api_key = 'csMXTrxkWJvnRMvy7SmkBrNwa'
+api_secret = '80jU1i6jLvBC0tFqJZhNHuYgjwTyqGGKzKRp50yRzdJiic1H7K'
+access_token = '223026744-WEMrzubOKlh9MvxuuFR7NLE5qQaJW1prhujcPuzK'
+access_secret = 'ZLhjdWTEGbe2Gb1ZQAtCAItT35f18HkOyerJaJPx7ouU0'
+
+# import dependencies
+from confluent_kafka import Producer
+from faker import Faker
 import json
+import time
+import logging
+import random
+import tweepy
 
-"""API ACCESS KEYS"""
+# def tweepy
+client = tweepy.Client(bearer_token, api_key, api_secret, access_token, access_secret)
+auth = tweepy.OAuth1UserHandler (api_key, api_secret, access_token, access_secret)
+api = tweepy.API(auth)
+search = ['valorant league', 'valorant franchising']
 
-consumerKey = "csMXTrxkWJvnRMvy7SmkBrNwa"
-consumerSecret = "80jU1i6jLvBC0tFqJZhNHuYgjwTyqGGKzKRp50yRzdJiic1H7K"
-accessToken = "223026744-WEMrzubOKlh9MvxuuFR7NLE5qQaJW1prhujcPuzK"
-accessTokenSecret = "ZLhjdWTEGbe2Gb1ZQAtCAItT35f18HkOyerJaJPx7ouU0"
+# def logging
+logging.basicConfig(format='%(asctime)s %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    filename='producer.log',
+                    filemode='w')
 
-producer = KafkaProducer(bootstrap_servers='localhost:9092')
-search_term = 'Valorant League'
-topic_name = 'twitter'
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
+# def producer
+p=Producer({'bootstrap.servers':'localhost:9092'})
+print('Kafka Producer has been initiated...')
 
-def twitterAuth():
-    # create the authentication object
-    authenticate = tweepy.OAuthHandler(consumerKey, consumerSecret)
-    # set the access token and the access token secret
-    authenticate.set_access_token(accessToken, accessTokenSecret)
-    # create the API object
-    api = tweepy.API(authenticate, wait_on_rate_limit=True)
-    return api
+# def receipt
+def receipt(err,msg):
+    if err is not None:
+        print('Error: {}'.format(err))
+    else:
+        message = 'Produced message on topic {} with value of {}\n'.format(msg.topic(), msg.value().decode('utf-8'))
+        logger.info(message)
+        print(message)
+        
+# def stream tweet
+class MyStream(tweepy.StreamingClient):
+    def on_tweet(self, tweet):
+        if tweet.referenced_tweets == None:
+            # print(tweet.text)
+            p.poll(1)
+            p.produce('twt_streaming', tweet.text, callback=receipt)
+            p.flush()
+            time.sleep(0.2)
+    def on_connect(self):
+        print('Connected')
 
+# def instance
+stream = MyStream(bearer_token= bearer_token)
 
-class TweetListener(tweepy.Stream):
+for term in search:
+    stream.add_rules(tweepy.StreamRule(term))
 
-    def on_data(self, raw_data):
-        logging.info(raw_data)
-        producer.send(topic_name, value=raw_data)
-        return True
-
-    def on_error(self, status_code):
-        if status_code == 420:
-            # returning False in on_data disconnects the stream
-            return False
-
-    def start_streaming_tweets(self, search_term):
-        self.filter(track=search_term, stall_warnings=True, languages=["en"])
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    twitter_stream = TweetListener(consumerKey, consumerSecret, accessToken, accessTokenSecret)
-    twitter_stream.start_streaming_tweets(search_term)
+stream.filter(tweet_fields=["referenced_tweets"])
